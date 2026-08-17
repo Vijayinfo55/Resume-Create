09c878fb22b07a120cc12057add014dfb42f1218
Vijayinfo55 <vijayinfo.verma55@gmail.com>
2026-08-17 11:36:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ user_info = st.text_area("""Write your Resume Description:""")
 
 #=========================
 model = ChatGoogleGenerativeAI(
-    model = 'gemini-3.6-flash',
+    model = 'gemini-3.5-flash',
 #   model = 'gemini-3.5-flash-lite',
     google_api_key = GOOGLE_API_KEY
 )
